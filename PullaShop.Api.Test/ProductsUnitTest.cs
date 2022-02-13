7c4bb9823b1e807c7367a854182525027c66f72f
Ville Kähärä <kahara.ville@gmail.com>
2022-02-13 12:47:18 +0200

    add test get all products from product data mock

--- PullaShop.Api.Test/ProductsUnitTest.cs
+++ PullaShop.Api.Test/ProductsUnitTest.cs
@@ -1,6 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 using PullaShop.Api.Models;
+using Moq;
 
 namespace PullaShop.Api.Test;
 
@@ -8,27 +9,30 @@ namespace PullaShop.Api.Test;
 public class ProductsUnitTest
 {
     [TestMethod]
-    public void TestGetAllProducts()
+    public void GetAllProductsReturnsAllProducts()
     {
-        var productData = new ProductData();
 
-        //Get 
-        var allProducts = productData.GetAllProducts();
+        // Arrange
+        var initialProduct = new ProductModel { Name = "Test Product" };
+        var mock = new Mock(IProductData);
+        mock.Setup(product => product.GetAllProducts()).Returns(new List<ProductModel>() {initialProduct});
+        var productData = new ProductData(mock.Object);
+        
+        // Act
+        var actualAllProducts = productData.GetAllProducts();
 
-        var expectedProductCount = 1; 
-
-        Assert.AreEqual(expectedProductCount, allProducts);
+        // Assert
+        Assert.AreEqual(1, actualAllProducts.Count);
     }
 
     [TestMethod]
-    public void TestGetSingleProduct()
+    public void GetReturnsProduct()
     {
-        var productData = new ProductData();
 
-        var actualProduct = productData.GetProduct(0);
+        // var productData = new ProductData();
+        // var actualProduct = productData.GetProduct(0);
+        // var expectedProduct = new ProductModel { Name = "Test Product" };
 
-        var expectedProduct = new ProductModel { Name = "Test Product" };
-
-        Assert.AreEqual(expectedProduct.Name, actualProduct.Name);
+        // Assert.AreEqual(expectedProduct.Name, actualProduct.Name);
     }
 }
