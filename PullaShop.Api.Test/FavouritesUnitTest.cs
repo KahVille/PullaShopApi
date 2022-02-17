7307a3c860366b4dd4c29ab2d18178a082b6b758
Ville Kähärä <kahara.ville@gmail.com>
2022-02-17 11:59:31 +0200

    test add to favourites

---
+++ PullaShop.Api.Test/FavouritesUnitTest.cs
@@ -0,0 +1,48 @@
+using Xunit;
+using System.Collections.Generic;
+using PullaShop.Api.Models;
+using Moq;
+using PullaShop.Api.DataAccess.Data;
+using System.Threading.Tasks;
+using PullaShop.Api.DataAccess.DatabaseAccess;
+using System.Linq;
+using System.Text.Json;
+using System;
+
+namespace PullaShop.Api.Test;
+
+public class FavouritesUnitTest
+{
+
+    [Fact]
+    public async Task GetAllFavouriteProductsReturnsAllFavouriteProducts()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task AddFavouriteProductsToFavaourites() {
+
+        // Arrange
+        var expectedFavourites = GetSampleProducts();
+        string sql = "";
+        var databaseMock = new Mock<ISqlDataAccess>();
+        databaseMock.Setup(dataAccess => dataAccess.MyConnectionString).Returns("Default");
+        databaseMock.Setup(dataAccess => dataAccess.SaveData<List<ProductModel>>(sql)).ReturnsAsync(GetSampleProducts());
+
+        // Act
+        var favouritesData = new FavouriteData(databaseMock);
+        var actualSavedFavourites = await favouritesData.AddFavouriteProducts(It.IsAny<List<ProductModel>>);
+
+        // Assert
+        Assert.Equal(expectedFavourites.Count, actualSavedFavourites.Count);
+
+    }
+
+    private List<ProductModel> GetSampleProducts()
+    {
+        return new List<ProductModel>() {
+            new ProductModel { Id= 0, Name = "Test Product" }
+        };
+    }
+
+}
